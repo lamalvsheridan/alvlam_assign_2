d5b60d3e07178f2459c0aa1422dca46618ff6790
Alvin Lam <105746805+lamalvsheridan@users.noreply.github.com>
2022-10-25 23:26:08 -0400

    Blog Functionality v0.1
    For part 1. Only follows the module. Is not configured for the assignment.

--- blog/admin.py
+++ blog/admin.py
@@ -4,13 +4,23 @@ from . import models
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'title',
+        'author',
         'created',
         'updated',
     )
 
+    list_filter = (
+        'status',
+    )
+
     search_fields = (
         'title',
+        'author__username',
+        'author__first_name',
+        'author__last_name',
     )
+
+    prepopulated_fields = {'slug': ('title',)}
 
 # Register the `Post` model
 admin.site.register(models.Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings  # Imports Django's loaded settings
 from django.db import models
 
 # Create your models here.
@@ -5,15 +6,47 @@ class Post(models.Model):
     """
     Represents a blog post
     """
+    DRAFT = 'draft'
+    PUBLISHED = 'published'
+
+    STATUS_CHOICES = [
+        (DRAFT, 'Draft'),
+        (PUBLISHED, 'Published')
+    ]
+
     title = models.CharField(max_length=255)
+
+    slug = models.SlugField(
+        null=False,
+        unique_for_date='published',
+    )
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,  # The Django auth user model
+        on_delete=models.PROTECT,  # Prevent posts from being deleted
+        related_name='blog_posts',
+        null=False,
+    )
+
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default=DRAFT,
+        help_text='Set to "published" to make this post publicly visible',
+    )
+
     content = models.TextField()
+
+    published = models.DateTimeField(
+        null=True,
+        blank=True,
+        help_text='The date & time this article was published'
+    )
+
     created = models.DateTimeField(auto_now_add=True)  # Sets on create
     updated = models.DateTimeField(auto_now=True)  # Updates on each save
 
     class Meta:
-        # Sort by the `created` field. The `-` prefix
-        # specifies to order in descending/reverse order.
-        # Otherwise, it will be in ascending order.
         ordering = ['-created']
 
     def __str__(self):
