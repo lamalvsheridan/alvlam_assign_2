c23e4400b0cea8106dbd3e1af1db824191d12681
Alvin Lam <105746805+lamalvsheridan@users.noreply.github.com>
2022-10-26 03:38:58 -0400

    V1.0
    Completed all functionality

--- blog/admin.py
+++ blog/admin.py
@@ -1,16 +1,45 @@
 from django.contrib import admin
 from . import models
 
+class CommentInline(admin.StackedInline):
+    model = models.Comment
+
+    exclude = (
+        'created',
+        'updated',
+    )
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    can_delete = False
+
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+    )
+
+    prepopulated_fields = {'slug': ('name',)}
+
+
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'title',
-        'author',
         'created',
         'updated',
     )
 
     list_filter = (
         'status',
+        'topic',
     )
 
     search_fields = (
@@ -20,7 +49,37 @@ class PostAdmin(admin.ModelAdmin):
         'author__last_name',
     )
 
+    inlines = [
+        CommentInline,
+    ]
+
     prepopulated_fields = {'slug': ('title',)}
 
-# Register the `Post` model
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'email',
+        'post',
+        'created',
+        'updated',
+        'approved',
+    )
+
+    list_filter = (
+        'approved',
+    )
+
+    search_fields = (
+        'name',
+        'email',
+        'post',
+        'text',
+    )
+
+
+
+# Register models
+admin.site.register(models.Topic, TopicAdmin)
 admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.Comment, CommentAdmin)
